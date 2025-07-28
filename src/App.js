6b273fb310dc0e96a75f12d8bd3e4efeb8fc8ccb
choiWebdev <choi.webdev@gmail.com>
2025-07-28 22:18:10 +0900

    스타일 프레임 잡는 중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="logo">
+          <a href="*">
+            캘린더
+          </a>
+        </h1>
       </header>
+      <div className="month">
+        2025.07
+      </div>
     </div>
   );
 }
